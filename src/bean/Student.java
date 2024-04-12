332eb48191fd643b4c755dc192259deeccc0723c
oohara <oohara@172.16.31.92>
2024-04-12 15:17:48 +0900

    04/12

--- src/bean/Student.java
+++ src/bean/Student.java
@@ -2,30 +2,42 @@ package bean;
 
 import java.io.Serializable;
 
-public class Teacher implements Serializable {
-
-	private String id;
-	private String password;
+public class Student implements Serializable {
+	private String no;
 	private String name;
+	private int entYear;
+	private String classNum;
+	private boolean isAttend;
 	private School school;
-
-	public String setid() {
-		return id;
+	public String getNo() {
+		return no;
 	}
-	public void setid(String id) {
-		this.id = id;
+	public void setNo(String no) {
+		this.no = no;
 	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String setName() {
+	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public int getEntYear() {
+		return entYear;
+	}
+	public void setEntYear(int entYear) {
+		this.entYear = entYear;
+	}
+	public String getClassNum() {
+		return classNum;
+	}
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
+	}
+	public boolean isAttend() {
+		return isAttend;
+	}
+	public void setAttend(boolean isAttend) {
+		this.isAttend = isAttend;
 	}
 	public School getSchool() {
 		return school;
@@ -33,4 +45,6 @@ public class Teacher implements Serializable {
 	public void setSchool(School school) {
 		this.school = school;
 	}
+
+
 }
