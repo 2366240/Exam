235554b6eb526417b5f4a6aea99cc7eb58a8e0b1
oohara <oohara@172.16.31.96>
2024-04-17 12:49:10 +0900

    4/17

--- src/scoremanager/main/StudentListAction.java
+++ src/scoremanager/main/StudentListAction.java
@@ -2,11 +2,13 @@ package scoremanager.main;
 
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.School;
 import bean.Student;
 import bean.Teacher;
 import dao.StudentDao;
@@ -21,6 +23,18 @@ public class StudentListAction extends Action{
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception{
 		HttpSession session = request.getSession();//TODO 自動生成されたメゾット・スタブ
 		Teacher teacher = (Teacher)session.getAttribute("user");
+		//テスト用に学校オブジェクトと講師オブジェクトを一次的に作る
+		School school = new School();
+		school.setCd("tes");
+		school.setName("テスト校");
+
+		Teacher teacher = new Teacher();
+		teacher.setId("admin1");
+		teacher.setName("管理者1");
+		teacher.setPassword("password");
+		teacher.setSchool(school);
+
+
 
 		String entYearStr=""; //入力された入学年度
 		String classNum = ""; //入力されたクラス番号
@@ -56,6 +70,37 @@ public class StudentListAction extends Action{
 		errors.put("f1", "クラス指定する場合は入学年度も指定してください");
 		req.setAttribute("errors", errors);
 		//全学生情報を取得
-		student = sDao.filter(teacher.getSchool(), isAttend);
+		students = sDao.filter(teacher.getSchool(), isAttend);
+	}
+	//ビジネスロック
+	if (entYearStr !=null) {
+		//数値を変換
+		entYear = Integer.parseInt(entYearStr);
+	}
+	//リストを初期化
+	List<Integer> entYearSet = new ArrayList<>();
+	//10年前から１年後まで年をリストに追加
+	for (int i = year -10; i < year +1; i++) {
+		entYearSet.add(i);
+	}
+	//レスポンス値をセット6
+	//リスエストに入学年度をセット
+	req.setAttribute("f1" , entYear);
+	//リクエストにクラス番号をセット
+	req.setAttribute("f2" , classNum);
+	//在学フラグが送信されていた場合
+	if(isAttentStr != null) {
+		//在学フラグを立てる
+		isAttent = true;
+		//リクエストに在学フラグをセット
+		req.setAttribute("f3", isAttendStr);
+	}
+	//リクエストに学生リストをセット
+	req.setAttrinute("students" , students);
+	//リスエストにデータをセット
+	req.setAttribute("class_num_set" , list);
+	req.setAttribute("ent_year_set" , entYearSet);
+	//JSPへフォワード7
+	req.getRequestDispatcher("student_list.jsp").forward(req, res;)
 	}
 }
--- src/tool/FrontController.java
+++ src/tool/FrontController.java
@@ -2,6 +2,7 @@ package tool;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
@@ -12,14 +13,40 @@ import javax.servlet.http.HttpServletResponse;
 /**
 *Servlet implementation class FrontController
 */
-@WebServlet("/FrontController")
-public class FrontController extende HttlServlet {
+@WebServlet(urlPatterns = {"*.action"})
+public class FrontController extends HttpServlet {
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequst requst, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest requst, HttpServletResponse response) throws ServletException,IOException{
-		//TODO Auto-generated method stub
+		try{
+			//パスを取得
+			String path = requst.getServletPath().substring(1);
+			//ファイル名を取得しクラス名に変換
+			String name = path.replace(".a", "A").replace('/', '.');
+
+			System.out.println("★ servlet path ->" + requst.getServletPath());
+			System.out.println("★ class name ->" + name);
+
+			//アクションクラスのインスタンスを返却
+			Action action = (Action) Class.forName(name).getDeclaredConstructor().newInstance();
+
+			//移先URLを取得
+			action.execute(request, respons);
+			//String url = action.execute(requst, response);
+			//requst.getRequestDispatcher(url).forward(requst, response);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			//エラーページへリダイレクト
+			requst.getRequestDispatcher("error.jsp").forward(requst, response);
+		}
+	}
+	@Override
+	protectsd void doPost(HttpServletRequest requst, HttpServletResponse response) throws Servletexception, IOException{
+		doGet(requst,response);
+	}
 		response.getWriter().append("Served at:").append(request.getContextPath());
 	}
 
